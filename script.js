0e80dc1deaed5eefde349c010f3f7e2828bbd42f
Jen Anderson <jnn17736@gmail.com>
2016-02-16 19:49:21 -0500

    Week 7 Commits

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+    $(document).ready(function () {
+        $('#nav li').hover(
+        function () {
+            //show submenu
+            $('ul', this).show();
+        }, function () {
+            //hide submenu
+            $('ul', this).hide();
+        });
+    });
+</script>
+ 
